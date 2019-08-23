61ba51786f631db5ef706a8e97a15695714cc05c
abisalehalliprasan <anil_kumar3@intuit.com>
2019-08-23 15:29:12 -0700

    ES6 Standard Adoption

---
+++ CHANGELOG.md
@@ -0,0 +1,49 @@
+# Changelog
+
+## [1.5.0](https://github.com/intuit/oauth-jsclient/releases/tag/1.5.0)
+
+- ES6 Standard Adoption 
+
+## [1.4.0](https://github.com/intuit/oauth-jsclient/releases/tag/1.4.0)
+
+- Clear Token Object on invoking revoke() functionality
+
+## [1.3.0](https://github.com/intuit/oauth-jsclient/releases/tag/1.3.0)
+
+- TokenValidation for Revoke functionality Fixed
+  
+## [1.2.0](https://github.com/intuit/oauth-jsclient/releases/tag/1.2.0)
+
+- Highly Improved Implementation : setToken functionality  
+
+## [1.1.3](https://github.com/intuit/oauth-jsclient/releases/tag/1.1.3)
+
+- Setting the Token methodology fixed
+
+## [1.1.2](https://github.com/intuit/oauth-jsclient/releases/tag/1.1.2)
+
+- Supports Token Setting Functionality + New Scopes added
+
+## [1.1.1](https://github.com/intuit/oauth-jsclient/releases/tag/1.1.1)
+
+- Rolling Back changes to realmId field on createToken()
+
+## [1.1.0](https://github.com/intuit/oauth-jsclient/releases/tag/1.1.0)
+
+- Support for passing realmId and id_token using setToken()
+
+## [1.0.3](https://github.com/intuit/oauth-jsclient/releases/tag/1.0.3)
+
+- Support for RefreshUsingToken method
+
+## [1.0.2](https://github.com/intuit/oauth-jsclient/releases/tag/1.0.2)
+
+- Version Release -  1.0.2
+
+## [1.0.1](https://github.com/intuit/oauth-jsclient/releases/tag/1.0.1)
+
+- npm publish patch - 1.0.1
+
+## [1.0.0](https://github.com/intuit/oauth-jsclient/releases/tag/1.0.0)
+
+- First Release - 1.0.0
--- src/response/AuthResponse.js
+++ src/response/AuthResponse.js
@@ -20,6 +20,8 @@
  * @namespace AuthResponse
  */
 
+'use strict';
+
 /**
  * AuthResponse
  * @property {Token} token
@@ -29,65 +31,61 @@
  * @property {string} intuit_tid
  */
 function AuthResponse(params) {
-
-    this.token = params.token || '';
-    this.response = params.response || '';
-    this.body = params.responseText || '';
-    this.json = null;
-    this.intuit_tid = params.intuit_tid || '';
+  this.token = params.token || '';
+  this.response = params.response || '';
+  this.body = params.responseText || '';
+  this.json = null;
+  this.intuit_tid = params.intuit_tid || '';
 }
 
 /**
  * Process Response
  * @param response
  */
-AuthResponse.prototype.processResponse = function(response) {
-
-    this.response = response || '';
-    this.body = response.body || '';
-    this.json = this.body ? JSON.parse(this.body) : null;
-    this.intuit_tid = response.headers['intuit_tid'] || '';
-}
+AuthResponse.prototype.processResponse = function (response) {
+  this.response = response || '';
+  this.body = response.body || '';
+  this.json = this.body ? JSON.parse(this.body) : null;
+  this.intuit_tid = response.headers.intuit_tid || '';
+};
 
 /**
  * @return {AuthResponse}
  */
-AuthResponse.prototype.getToken = function() {
-    return this.token.getToken();
+AuthResponse.prototype.getToken = function () {
+  return this.token.getToken();
 };
 
 
-
 /**
  * @return {AuthResponse}
  */
-AuthResponse.prototype.text = function() {
-    return this.body;
+AuthResponse.prototype.text = function () {
+  return this.body;
 };
 
 /**
  *
  * @returns {*}
  */
-AuthResponse.prototype.status = function() {
-    return this.response.status;
+AuthResponse.prototype.status = function () {
+  return this.response.status;
 };
 
 /**
  * Get response headers
  * @returns {Object} headers
  */
-AuthResponse.prototype.headers = function() {
-    return this.response.headers;
+AuthResponse.prototype.headers = function () {
+  return this.response.headers;
 };
 
 /**
  * valid { response is valid ? }
  * @returns {*|boolean}
  */
-AuthResponse.prototype.valid = function() {
-
-    return (this.response && this.response.status == '200');
+AuthResponse.prototype.valid = function () {
+  return (this.response && this.response.status == '200');
 };
 
 
@@ -95,43 +93,42 @@ AuthResponse.prototype.valid = function() {
  * Get Json () { returns token as JSON }
  * @return {object} this.json
  */
-AuthResponse.prototype.getJson = function() {
-    if (!this.isJson()) throw new Error('AuthResponse is not JSON');
-    if (!this.json) {
-        this.json = this.body ? JSON.parse(this.body) : null;
-    }
-    return this.json;
+AuthResponse.prototype.getJson = function () {
+  if (!this.isJson()) throw new Error('AuthResponse is not JSON');
+  if (!this.json) {
+    this.json = this.body ? JSON.parse(this.body) : null;
+  }
+  return this.json;
 };
 
 /**
  * Get Intuit tid
  * @returns {string} intuit_tid
  */
-AuthResponse.prototype.get_intuit_tid = function() {
-
-    return this.intuit_tid;
+AuthResponse.prototype.get_intuit_tid = function () {
+  return this.intuit_tid;
 };
 
 
 /**
  * @private
  */
-AuthResponse.prototype.isContentType = function(contentType) {
-    return this.getContentType().indexOf(contentType) > -1;
+AuthResponse.prototype.isContentType = function (contentType) {
+  return this.getContentType().indexOf(contentType) > -1;
 };
 
 /**
  * @private
  */
-AuthResponse.prototype.getContentType = function() {
-    return this.response.get(AuthResponse._contentType) || '';
+AuthResponse.prototype.getContentType = function () {
+  return this.response.get(AuthResponse._contentType) || '';
 };
 
 /**
  * @private
  */
-AuthResponse.prototype.isJson = function() {
-    return this.isContentType('application/json');
+AuthResponse.prototype.isJson = function () {
+  return this.isContentType('application/json');
 };
 
 
