afbcc0399d5de3c368c5a645aa5ac7f11f569455
abisalehalliprasan <anil_kumar3@intuit.com>
2019-09-30 12:46:51 -0700

    Squashing Commits to single commit

--- test/TokenTest.js
+++ test/TokenTest.js
@@ -1,99 +1,87 @@
 'use strict';
 
-var { expect } = require('chai');
+const { expect } = require('chai');
 
-var OAuthClientTest = require('../src/OAuthClient');
-var expectedAccessToken = require('./mocks/bearer-token.json');
+const OAuthClientTest = require('../src/OAuthClient');
+const expectedAccessToken = require('./mocks/bearer-token.json');
 
 
-
-var oauthClient = new OAuthClientTest({
-    clientId: 'clientID',
-    clientSecret: 'clientSecret',
-    environment: 'sandbox',
-    redirectUri: 'http://localhost:8000/callback'
+const oauthClient = new OAuthClientTest({
+  clientId: 'clientID',
+  clientSecret: 'clientSecret',
+  environment: 'sandbox',
+  redirectUri: 'http://localhost:8000/callback',
 });
 
-describe('Tests for Token', function() {
+describe('Tests for Token', () => {
+  it('Creates a new token instance', () => {
+    const token = oauthClient.getToken();
+    expect(token).to.have.property('realmId');
+    expect(token).to.have.property('token_type');
+    expect(token).to.have.property('access_token');
+    expect(token).to.have.property('refresh_token');
+    expect(token).to.have.property('expires_in');
+    expect(token).to.have.property('x_refresh_token_expires_in');
+    expect(token).to.have.property('latency');
+    expect(token).to.have.property('id_token');
+  });
 
-    it('Creates a new token instance', function() {
-
-        var token = oauthClient.getToken();
-        expect(token).to.have.property('realmId');
-        expect(token).to.have.property('token_type');
-        expect(token).to.have.property('access_token');
-        expect(token).to.have.property('refresh_token');
-        expect(token).to.have.property('expires_in');
-        expect(token).to.have.property('x_refresh_token_expires_in');
-        expect(token).to.have.property('latency');
-        expect(token).to.have.property('id_token');
+  it('Set Token using Constructor', () => {
+    const oauthClient = new OAuthClientTest({
+      clientId: 'clientID',
+      clientSecret: 'clientSecret',
+      environment: 'sandbox',
+      redirectUri: 'http://localhost:8000/callback',
+      token: expectedAccessToken,
     });
+    const token = oauthClient.getToken();
 
-    it('Set Token using Constructor', function() {
-        var oauthClient = new OAuthClientTest({
-            clientId: 'clientID',
-            clientSecret: 'clientSecret',
-            environment: 'sandbox',
-            redirectUri: 'http://localhost:8000/callback',
-            token: expectedAccessToken
-        });
-        var token = oauthClient.getToken();
+    expect(token.access_token).to.equal(expectedAccessToken.access_token);
+    expect(token.refresh_token).to.equal(expectedAccessToken.refresh_token);
+    expect(token.token_type).to.equal(expectedAccessToken.token_type);
+    expect(token.expires_in).to.equal(expectedAccessToken.expires_in);
+    expect(token.x_refresh_token_expires_in).to.equal(expectedAccessToken.x_refresh_token_expires_in);
+  });
 
-        expect(token.access_token).to.equal(expectedAccessToken.access_token);
-        expect(token.refresh_token).to.equal(expectedAccessToken.refresh_token);
-        expect(token.token_type).to.equal(expectedAccessToken.token_type);
-        expect(token.expires_in).to.equal(expectedAccessToken.expires_in);
-        expect(token.x_refresh_token_expires_in).to.equal(expectedAccessToken.x_refresh_token_expires_in);
+  it('Set Token using Helper Method', () => {
+    oauthClient.token.setToken(expectedAccessToken);
+    const token = oauthClient.getToken();
 
-    })
+    expect(token.access_token).to.equal(expectedAccessToken.access_token);
+    expect(token.refresh_token).to.equal(expectedAccessToken.refresh_token);
+    expect(token.token_type).to.equal(expectedAccessToken.token_type);
+    expect(token.expires_in).to.equal(expectedAccessToken.expires_in);
+    expect(token.x_refresh_token_expires_in).to.equal(expectedAccessToken.x_refresh_token_expires_in);
+  });
 
-    it('Set Token using Helper Method', function() {
+  it('Get Access Token using Helper Method', () => {
+    oauthClient.token.setToken(expectedAccessToken);
+    const access_token = oauthClient.getToken().accessToken();
 
-        oauthClient.token.setToken(expectedAccessToken);
-        var token = oauthClient.getToken();
-
-        expect(token.access_token).to.equal(expectedAccessToken.access_token);
-        expect(token.refresh_token).to.equal(expectedAccessToken.refresh_token);
-        expect(token.token_type).to.equal(expectedAccessToken.token_type);
-        expect(token.expires_in).to.equal(expectedAccessToken.expires_in);
-        expect(token.x_refresh_token_expires_in).to.equal(expectedAccessToken.x_refresh_token_expires_in);
-
-    });
-
-    it('Get Access Token using Helper Method', function() {
-
-        oauthClient.token.setToken(expectedAccessToken);
-        var access_token = oauthClient.getToken().accessToken();
-
-        expect(access_token).to.deep.equal(expectedAccessToken.access_token);
-    });
+    expect(access_token).to.deep.equal(expectedAccessToken.access_token);
+  });
 
 
-    it('Get Refresh Token using Helper Method', function() {
+  it('Get Refresh Token using Helper Method', () => {
+    oauthClient.token.setToken(expectedAccessToken);
+    const refresh_token = oauthClient.getToken().refreshToken();
 
-        oauthClient.token.setToken(expectedAccessToken);
-        var refresh_token = oauthClient.getToken().refreshToken();
+    expect(refresh_token).to.deep.equal(expectedAccessToken.refresh_token);
+  });
 
-        expect(refresh_token).to.deep.equal(expectedAccessToken.refresh_token);
-    });
+  it('Get TokenType using Helper Method', () => {
+    oauthClient.token.setToken(expectedAccessToken);
+    const token_type = oauthClient.getToken().tokenType();
 
-    it('Get TokenType using Helper Method', function() {
+    expect(token_type).to.deep.equal(expectedAccessToken.token_type);
+  });
 
-        oauthClient.token.setToken(expectedAccessToken);
-        var token_type = oauthClient.getToken().tokenType();
+  it('Get Token  using Helper Method', () => {
+    oauthClient.token.setToken(expectedAccessToken);
+    const token = oauthClient.getToken().getToken();
 
-        expect(token_type).to.deep.equal(expectedAccessToken.token_type);
-    });
-
-    it('Get Token  using Helper Method', function() {
-
-        oauthClient.token.setToken(expectedAccessToken);
-        var token = oauthClient.getToken().getToken()
-
-        expect(token).to.be.a('Object');
-        expect(token.access_token).to.deep.equal('sample_access_token');
-    });
-
+    expect(token).to.be.a('Object');
+    expect(token.access_token).to.deep.equal('sample_access_token');
+  });
 });
-
 
