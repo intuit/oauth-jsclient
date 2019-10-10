90cfde41978c05bf080cbc23f171945ec8676270
Leeland Clenista <31978878+Rockleeland@users.noreply.github.com>
2019-10-09 21:09:35 -0700

    added test for 'clearTest' function (#44)
    * fixed camelcase issue and added disable camelcase in files that might have needed it for backwards compatibility
    
    * added test for clear token
    
    * added test for clearToken

--- test/TokenTest.js
+++ test/TokenTest.js
@@ -1,3 +1,5 @@
+/* eslint-disable camelcase */
+
 'use strict';
 
 const { describe, it } = require('mocha');
@@ -79,12 +81,23 @@ describe('Tests for Token', () => {
     expect(token_type).to.deep.equal(expectedAccessToken.token_type);
   });
 
-  it('Get Token  using Helper Method', () => {
+  it('Get Token using Helper Method', () => {
     oauthClient.token.setToken(expectedAccessToken);
     const token = oauthClient.getToken().getToken();
 
     expect(token).to.be.a('Object');
     expect(token.access_token).to.deep.equal('sample_access_token');
+  });
+
+  it('Clear Token using Helper Method', () => {
+    oauthClient.token.setToken(expectedAccessToken);
+    const token = oauthClient.getToken().clearToken();
+
+    expect(token.access_token).to.equal('');
+    expect(token.refresh_token).to.equal('');
+    expect(token.token_type).to.equal('');
+    expect(token.expires_in).to.equal(0);
+    expect(token.x_refresh_token_expires_in).to.equal(0);
   });
 });
 
