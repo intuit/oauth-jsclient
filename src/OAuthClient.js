db9a3ec9b5bf13dab5e6f67b1707bee81923d55d
Rajesh Gupta <rajesh_gupta2@intuit.com>
2024-08-26 00:19:54 -0700

    added check for property json on resp obj

--- src/OAuthClient.js
+++ src/OAuthClient.js
@@ -182,10 +182,10 @@ OAuthClient.prototype.createToken = function createToken(uri) {
     resolve(this.getTokenRequest(request));
   })
     .then((res) => {
-      const authResponse = res.json ? res : null;
+      const authResponse = res.hasOwnProperty('json')? res : null;
       const json = (authResponse && authResponse.json) || res;
       this.token.setToken(json);
-       this.log('info', 'Create Token response is : ', JSON.stringify(authResponse && authResponse.json, null, 2));
+      this.log('info', 'Create Token response is : ', JSON.stringify(authResponse && authResponse.json, null, 2));
       return authResponse;
     })
     .catch((e) => {
@@ -223,7 +223,7 @@ OAuthClient.prototype.refresh = function refresh() {
     resolve(this.getTokenRequest(request));
   })
     .then((res) => {
-      const authResponse = res.json ? res : null;
+      const authResponse = res.hasOwnProperty('json')? res : null;
       const json = (authResponse && authResponse.json) || res;
       this.token.setToken(json);
       this.log('info', 'Refresh Token () response is : ', JSON.stringify(authResponse && authResponse.json, null, 2));
@@ -265,7 +265,7 @@ OAuthClient.prototype.refreshUsingToken = function refreshUsingToken(refresh_tok
     resolve(this.getTokenRequest(request));
   })
     .then((res) => {
-      const authResponse = res.json ? res : null;
+      const authResponse = res.hasOwnProperty('json')? res : null;
       const json = (authResponse && authResponse.json) || res;
       this.token.setToken(json);
       this.log(
@@ -315,7 +315,7 @@ OAuthClient.prototype.revoke = function revoke(params) {
     resolve(this.getTokenRequest(request));
   })
     .then((res) => {
-      const authResponse = res.json ? res : null;
+      const authResponse = res.hasOwnProperty('json')? res : null;
       this.token.clearToken();
       this.log('info', 'Revoke Token () response is : ', JSON.stringify(authResponse && authResponse.json, null, 2));
       return authResponse;
@@ -349,7 +349,7 @@ OAuthClient.prototype.getUserInfo = function getUserInfo() {
     resolve(this.getTokenRequest(request));
   })
     .then((res) => {
-      const authResponse = res.json ? res : null;
+      const authResponse = res.hasOwnProperty('json')? res : null;
       this.log(
         'info',
         'The Get User Info () response is : ', JSON.stringify(authResponse && authResponse.json, null, 2),
